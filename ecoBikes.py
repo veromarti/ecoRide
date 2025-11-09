b05d3aee2997d72bd4a1306af8f0398693dcdb83
Veronica Martinez Cadavid <veromar.c96@gmail.com>
2025-11-08 20:39:06 -0500

    Update ecoBikes.py

--- ecoBikes.py
+++ ecoBikes.py
@@ -5,6 +5,7 @@ precios_bikes=[250, 500, 400, 200]
 
 servicio=False
 usuario = False
+menu = False
 cont = 1
 
 dict_usuario = {
@@ -13,7 +14,8 @@ dict_usuario = {
     "Servicio " + str(cont):{"Bicleta": "",
     "Tiempo": 0,
     "Tarifa por minuto": 0.0,
-    "Costo bicicleta": 0},
+    "Costo bicicleta": 0,
+    "Tiempo de uso":0},
     "Metodo de Pago":"",
     "Costo Total": 0,
     "Descuentos": 0,
@@ -21,13 +23,13 @@ dict_usuario = {
 }
 
 def registro_usuario():
-    print("Ingrese su información de usuario:\n")
+    print("\nIngrese su información de usuario:\n")
     nombre = input("Nombre: ")
     telefono = input("Teléfono: ")
     dict_usuario["Usuario "]["Nombre"] = nombre
     dict_usuario["Usuario "]["Telefono"] = telefono
     user = True
-    print("\nUsuario registrado con exito.")
+    print("\nUsuario registrado con exito.\n")
     return user
 
 def crear_servicio(bicicleta, tiempo, num_servicio):
@@ -67,14 +69,18 @@ def mostrar_bikes():
     return opcion
 
 def mostrar_tarifas():
+    print("\n- - - - - Tarifas EcoRides - - - - -\n")
+
     for i in range(len(tipos_bikes)):
         print(str(i+1)+". " + tipos_bikes[i] + " - $" + str(precios_bikes[i]) + " por minuto")
-        
+    
+    opcion = (input("\nIngrese cualquier tecla para volver al Menu Principal ->"))
+    return opcion
 
 def mostrar_metodo():
     print("1. Efectivo\n2. Tarjeta \n3. Puntos\n4. Salir")
 
-    metodo_pago = input("\n¿Cuál método de pago deseas utilizar?: \n").strip()
+    metodo_pago = input("\n¿Cuál método de pago desea utilizar? (1-4): \n").strip()
 
     if metodo_pago == "1":
         metodo_pago = "Efectivo"
@@ -90,13 +96,22 @@ def mostrar_metodo():
 
     if metodo_pago:
         print(f"Método de pago seleccionado: {metodo_pago}") 
-        return metodo_pago
+        dict_usuario["Metodo de Pago"] = metodo_pago
+        return metodo_pago.lower()
     
-def descuentos(tiempo, tiempo_uso,metodo_pago):
+def tiempo_de_uso(num_pedidos):
+    for i in range(num_pedidos):
+        print("\nServicio " , i+1)
+        print(dict_usuario["Servicio " + str(i+1)])
+        tiempo_real = int(input("\nIngrese el tiempo real de uso para este servicio en minutos: ")) 
+        dict_usuario["Servicio " + str(i+1)]["Tiempo de uso"] = tiempo_real
     
-    #tarifa = dict_usuario["Servicio " + str(cont)]["Costo bicicleta"] 
+def descuentos(tiempo, tiempo_uso, metodo_pago):
+    
     fecha= date.today()
-    dia = fecha.weekday() #0 = Lunes 6= Domingo    
+    dia = fecha.weekday() #0 = Lunes 6= Domingo  
+    descuento = 0  
+    multa = 0
     
     if tiempo > 60 and metodo_pago == "tarjeta":
         descuento = 0.10
@@ -110,42 +125,70 @@ def descuentos(tiempo, tiempo_uso,metodo_pago):
 
     return descuento, recargo_finde, multa
 
-opcion = menu_principal()
+while not menu:
+    opcion = menu_principal()
 
-while not servicio:
-    
-    
     if opcion == 1:
+
         while not usuario:
             usuario = registro_usuario()
         
-        bicicleta = mostrar_bikes()
-        tiempo = int(input("Ingrese el tiempo de uso en minutos: "))
-        
-        crear_servicio(bicicleta, tiempo, cont)
-        
-        desea_agregar = input("\nDesea agregar otro servicio -> (y/n): ")
-        if desea_agregar.lower() == "y":
-            cont += 1            
-            servicio = False
+        while not servicio:
+            bicicleta = mostrar_bikes()
+            tiempo = int(input("Ingrese el tiempo de uso en minutos: "))
             
-        elif desea_agregar.lower() == "n":
-            print("\n - - - Gracias por usar el sericio de alquiler - - -")
-            servicio = True   
-        
+            crear_servicio(bicicleta, tiempo, cont)
+            desea_agregar = input("\nDesea agregar otro servicio -> (y/n): ")
+            print("\n")
+
+            if desea_agregar.lower() == "y":
+                cont += 1            
+                servicio = False
+                
+            elif desea_agregar.lower() == "n":
+                print("- - - Gracias por usar el servicio de alquiler - - -")
+                servicio = True   
+
     elif opcion == 2:
         mostrar_tarifas()
-        
+
     elif opcion == 3: 
-        tiempo_real = int(input("Ingrese el tiempo real de uso en minutos: "))  
-        pago=mostrar_metodo()
-        dict_usuario["Metodo de Pago"] = pago
-        descuento, recargo_finde, multa = descuentos(tiempo, tiempo_real, pago)
+
+        if servicio:
+            
+            print("\n")
+            tiempo_de_uso(cont)
+            print(dict_usuario)
+            pago = mostrar_metodo()
+
+            for i in range(cont):
+                tiempo = dict_usuario["Servicio " + str(i+1)]["Tiempo"]
+                tiempo_real = dict_usuario["Servicio " + str(i+1)]["Tiempo de uso"]
+                descuento, recargo_finde, multa = descuentos(tiempo, tiempo_real, pago)
+                print(dict_usuario["Servicio " + str(i+1)]["Costo bicicleta"])
+                print(type(dict_usuario["Servicio " + str(i+1)]["Costo bicicleta"]))
+
+                valor_base = int(dict_usuario["Servicio " + str(i+1)]["Costo bicicleta"])
+                valor_pago = (valor_base * (1-descuento)) * (1+recargo_finde) + multa
+
+                print(tiempo)
+                print(tiempo_real)
+                print(descuento)
+                print(recargo_finde)
+                print(multa)
+                print(valor_base)
+                print(valor_pago)
+
+            # dict_usuario["Costo Total"] = valor_pago
+            # resumen_pedido()
         
-        valor_pago = (dict_usuario["Servicio " + str(cont)]["Costo bicicleta"] * (1-descuento))(1+recargo_finde) + multa
-        dict_usuario["Costo Total"] = valor_pago
-        resumen_pedido()
-    
-    print(dict_usuario)
-    
-    #pago = dicti(s1)llave(tarifa)*
+        else:
+            print("\nNo hay ningun servicio pendiente de pago\n")
+            
+
+    elif opcion == 4:
+        print ("\nGracias por usar el servicio EcoRides")
+        menu = True
+
+    else:
+        ("\nIngrese una opcion valida/n")
